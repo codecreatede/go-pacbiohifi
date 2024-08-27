c953ddfa6c659fcf8cbd2e138ea2295f840f633f
Gaurav Sablok <codeprog@icloud.com>
2024-08-27 11:17:44 +0200

    codecreatede
    Added the support for the fasta reads from Illumina also and also adding the support for the native checks of
    the pacbiohifi reads to the illumina reads either coming from HiC

--- gohifiprogiler.go
+++ gohifiprogiler.go
@@ -1,4 +1,5 @@
 /*
+
 Author Gaurav Sablok
 Universitat Potsdam Library
 Date: 2024-8-7
@@ -6,6 +7,8 @@ Date: 2024-8-7
 A pacbiohifi go profiler that takes the pacbiohifi reads, makes the mers according to the length,
 filters the mers according to the critera. This is supported witht a desktop application to see that
 if your reads have high profiled genomic mers that could hinder the graph assembly.
+Added the support for the fasta reads from Illumina also and also adding the support for the native checks of
+the pacbiohifi reads to the illumina reads either coming from HiC
 
 */
 
@@ -17,8 +20,9 @@ import (
 	"log"
 	"os"
 	"strings"
-	_ "github.com/go-sql-driver/mysql" // adding the mysql driver so that it can interact with HUGO as a frontend. 
-	"" // adding the support for the charts plotting
+	_ "github.com/go-sql-driver/mysql"
+	// adding the mysql driver so that it can interact with HUGO as a frontend.
+	// adding the support for the charts plotting
 )
 
 func main() {
@@ -28,6 +32,9 @@ func main() {
 	argsread := os.Args[1:]
 	argswrite := os.Args[2:]
 	argskmer := os.Args[3:]
+	argsinput1 := os.Args[4:]
+	argsinput2 := os.Args[5:]
+
 	if err != nil {
 		panic (err)
 		log.Fatal(err.Error())
@@ -39,6 +46,15 @@ func main() {
 		log.Fatal(err)
 		return
 	}
+	argsinput1 := os.Args[1:]
+	argsinput2 := os.Args[2:]
+	argsoutput1 := os.Args[3:]
+	argsoutput2 := os.Args[4:]
+	if argsinput1 || argsinput2 || argsinput3 || argsinput4 == nil {
+		panic(err)
+		log.Fatal(err.Error.new("The arguments cant be empty"))
+	}
+
 	readbuffer := bufio.NewScanner(readfile)
 	header := []string{}
 	sequences := []string{}
@@ -143,5 +159,59 @@ func main() {
 		return fp.Sync() // fsync
 		}
 	}
+
+	/*
+	  adding the support for the hybrid check for the assembly of the pacbiohifi and the illumina reads
+	 */
 		
+	open1 := bufio.NewScanner(readinput1)
+	open2 := bufio.NewScanner(readinput2)
+
+	openinputH1 := []string{}
+	openinputSeq1 := []string{}
+	openinputH2 := []string{}
+	openinputSeq2 := []string{}
+
+	for open1.Scan() {
+		line := open1.Text()
+		if strings.HasPrefix(line, "@") || strings.Contains(string(line), "length") {
+			openinputH1 = append(openinputH1, strings.Split(line, " ")[0])
+		}
+		if string(line[0]) == "A" || string(line[0]) == "T" || string(line[0]) == "G" || string(line[0]) == "C" {
+			openinputSeq1 = append(openinputSeq1, line)
+		}
+	}
+
+	for open2.Scan() {
+		line := open2.Text()
+		if strings.HasPrefix(line, "@") || strings.Contains(string(line), "length") {
+			openinputH1 = append(openinputH2, strings.Split(line, " ")[0])
+		}
+		if string(line[0]) == "A" || string(line[0]) == "T" || string(line[0]) == "G" || string(line[0]) == "C" {
+			openinputSeq2 = append(openinputSeq2, line)
+		}
+	}
+
+	balancedH1 := []string{}
+	balancedH2 := []string{}
+	balancedH1head := []string{}
+	balancedH2head := []string{}
+
+	for i := range openinputH1 {
+		if string(openinputH1[i]) == string(openinputH2[i]) {
+			balancedH1 = append(balancedH1, openinputH1[i])
+			balancedH2 = append(balancedH2, openinputH2[i])
+			balancedH1head = append(balancedH1head, openinputSeq1[i])
+			balancedH2head = append(balancedH2head, openinputSeq2[i])
+		}
+	}
+
+	checklen := len(balancedH1) == len(balancedH1)
+	if checklen != true {
+		panic(err.Error())
+		log.Fatal(Error.new("The balancer is uneven and the reads cant be assembled"))
+
+	} else {
+		fmt.Println("The balancer is equal")
+	}
 }
